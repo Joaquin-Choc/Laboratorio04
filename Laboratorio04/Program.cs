c72ff3d7ab2decbb854254f012a256b2d231ff70
joaquín Choc <joaquinchoc@gmail.com>
2023-03-18 11:38:38 -0600

    Resolver ejercicio 01

--- Laboratorio04/Program.cs
+++ Laboratorio04/Program.cs
@@ -4,9 +4,9 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Nombre: ");
-            Console.WriteLine("Carnet: ");
-            Console.WriteLine("Seccion: ");
+            Console.WriteLine("Nombre: Joaquín Raymundo Choc Salvador ");
+            Console.WriteLine("Carnet: 1280423 ");
+            Console.WriteLine("Seccion: 04 ");
         }
     }
 }
