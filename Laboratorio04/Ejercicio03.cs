75d1051f2ef4d77604a83ad1e258d4e11c61c452
joaquín Choc <joaquinchoc@gmail.com>
2023-03-25 13:31:12 -0600

    Resolver ejercicio 03

--- Laboratorio04/Ejercicio03.cs
+++ Laboratorio04/Ejercicio03.cs
@@ -1,16 +1,51 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Laboratorio04
-{
+{ 
+
     public class Ejercicio03
     {
-        public string Sumar(string num1, string num2) 
+        public string Sumar(string num1, string num2)
         {
-            return "";
-        }
-    }
-}
+            if ("0".Equals(num1))
+                return num2;
+
+            if ("0".Equals(num2))
+                return num1;
+
+            string result = "";
+
+            int i = num1.Length - 1, j = num2.Length - 1;
+            int acarreo = 0;
+
+            while (i >= 0 && j >= 0)
+            {
+                int r = acarreo + (num1[i] - '0') + (num2[j]-'0');
+                acarreo = r / 10;
+
+                result = (r % 10) + result;
+
+                i--;
+                j--;
+            }
+
+            while (i >= 0)
+            {
+                int r = acarreo + (num1[i] - '0');
+                acarreo = r / 10;
+
+                result = (r % 10) + result;
+
+                i--;
+            }
+
+            if (acarreo > 0)
+                result = acarreo + result;
+
+            return result;
+}    }   } 
